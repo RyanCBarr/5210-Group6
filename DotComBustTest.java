8d549f8fb71d35ea6e2a9af566d1cf7d6544f423
RyanCBarr <40365899+RyanCBarr@users.noreply.github.com>
2021-05-02 17:26:24 -0700

    Added Test to DotComBustTest

--- DotComBustTest.java
+++ DotComBustTest.java
@@ -1,5 +1,8 @@
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 import org.junit.jupiter.api.Test;
 
 class DotComBustTest {
@@ -20,13 +23,20 @@ class DotComBustTest {
 	}
 
 	@Test
-	void testFinishGame() {
-		fail("Not yet implemented");
-	}
-
-	@Test
 	void testMain() {
 		fail("Not yet implemented");
 	}
+	
+	@Test
+	void testFinishGame() {
+		DotComBust test = new DotComBust();
+		
+		final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+		System.setOut(new PrintStream(outContent));
+		
+		test.finishGame();
+		
+		assertEquals("All Dot Coms are dead! Your stock is now worthless.\nIt only took you 0 guesses.\nYou got out before your options sank.\n", outContent.toString());
+	}
+}​​​​​​
 
-}
